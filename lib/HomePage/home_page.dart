5de7093891285e5e4adf58b11a4747dd139081c5
Arafat <arafat2k30@gmail.com>
2025-02-03 18:27:16 +0600

    Map integrate

--- lib/HomePage/home_page.dart
+++ lib/HomePage/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -9,8 +10,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(23.6850, 90.3563),
+    zoom: 14.4746,
+  );
+  static const CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google Map'),
+      ),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+          initialCameraPosition: _kGooglePlex),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'HomePage/home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -31,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Google map practice '),
+      home: HomePage(),
     );
   }
 }
